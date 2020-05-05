28a48401041c7602e24e88ad463db9b78eb3812c
Unknown <abad.sethi@gmail.com>
2020-05-05 14:27:45 +0200

    init
    Client and service first build

--- Client_BlackJack/main.c
+++ Client_BlackJack/main.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include "zhelpers.h"
+#include <unistd.h>
+#include <string.h>
+#include <zmq.h>
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    void *context = zmq_ctx_new ();
+    void *pusher = zmq_socket (context, ZMQ_PUSH);
+    void *sub = zmq_socket(context, ZMQ_SUB);
+
+    zmq_connect(pusher, "tcp://benternet.pxl-ea-ict.be:24041");
+    zmq_connect(sub, "tcp://benternet.pxl-ea-ict.be:24042");
 }
